3069813e9c9a9d090f8f3779b3f47aa0fe478670
wdhays2 <wdhays2@charter.net>
2017-09-22 20:55:17 -0700

    wip

--- app/services/weather_data.rb
+++ app/services/weather_data.rb
@@ -12,21 +12,15 @@ class WeatherData
     @url = 'http://weather.ggy.uga.edu/data/archive/daily/2008/20080101.txt'
     @data = {}
     @raw_data = []
-    @reading_types = ['Max Wind Speed', 'Max Humidity', 'Max Temperature'].freeze
+    # @reading_types = ['Max Wind Speed', 'Max Humidity', 'Max Temperature'].freeze
   end
 
   def process
-    # do something
-
     # return data
     @raw_data = get_data_from_url
-# binding.pry
-    # parse_raw_data
-    @raw_data.each do |row|
-      item = row[0]
-      row.shift
-      @data["#{item}"] = row
-    end
+
+    parse_raw_data
+
     @data.to_json
   end
 
@@ -35,12 +29,28 @@ class WeatherData
     extract_data(data)
   end
 
-  # def parse_raw_data
+  def parse_raw_data
     # do something with raw data
-    # store it into @data as an array of hashes
+    # store it into @data as a hash of arrays
+    @raw_data.each do |row|
+      item = row[0]
+      row.shift
+      if row[0] == 'Max'
+        row.each_with_index do |cell, indx|
+          row[indx-1] += " #{cell}" if row[indx] == 'Time'
+          row[indx].delete
+          binding.pry
+          as=12
+        end
+      else (/[a-zA-z]/).match?(row[0])
+        item += " " + row[0]
+        row.shift
+      end
+      @data["#{item}"] = row
+    end
     # results = sort_results(@raw_data)
     # @data = calculate_results(results)
-  # end
+  end
 
   def extract_data(data)
     data.map do |line|
@@ -62,22 +72,22 @@ class WeatherData
 
   ### Data Calculations ###
 
-  def mean(array)
-    total = array.inject(0) { |sum, x| sum + x }
-    total.to_f / array.length
-  end
+  # def mean(array)
+  #   total = array.inject(0) { |sum, x| sum + x }
+  #   total.to_f / array.length
+  # end
 
-  def median(array)
-    array.sort!
-    length = array.length
-    if length.odd?
-      array[length / 2]
-    else
-      item1 = array[length / 2 - 1]
-      item2 = array[length / 2]
-      mean([item1, item2])
-    end
-  end
+  # def median(array)
+  #   array.sort!
+  #   length = array.length
+  #   if length.odd?
+  #     array[length / 2]
+  #   else
+  #     item1 = array[length / 2 - 1]
+  #     item2 = array[length / 2]
+  #     mean([item1, item2])
+  #   end
+  # end
 
   # def sort_results(data)
   #   wind = []
