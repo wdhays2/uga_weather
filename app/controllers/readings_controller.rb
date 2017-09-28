b617d1b9e49a7ecc39406d2f2c0e583a6679c039
wdhays2 <wdhays2@charter.net>
2017-09-27 18:22:29 -0700

    Go to the website if all 5 days are not in the database.

--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -3,26 +3,33 @@
 class ReadingsController < ApplicationController
   def show
     @readings = []
-    weather_date = params[:date].to_date - 2.days
+    @weather_date = params[:date].to_date - 2.days
 
     # fetch the data from the database if it exists
-    5.times do
-      @read = WeatherEntry.where(entered_on: weather_date.strftime('%Y%m%d'))
-      @readings << @read
-      weather_date += 1.day
-    end
-    @readings.reject!(&:empty?)
-    return @readings unless @readings[0].empty?
+    retrieve_from_database
+    return @readings unless @readings.empty?
 
     # otherwise, get it from the website
-    @readings = []
-    5.times do
-      wd = WeatherData.new(weather_date.strftime('%Y%m%d'))
-      readings = wd.process
-      @read = readings.values.map { |data| Reading.new(data) }
-      @readings << @read
-      weather_date += 1.day
-    end
+    retrieve_from_website
     @readings.reject!(&:empty?)
+  end
+
+  def retrieve_from_database
+    5.times do
+      read = WeatherEntry.where(entered_on: @weather_date.strftime('%Y%m%d'))
+      return if read.empty?
+      @readings << read
+      @weather_date += 1.day
+    end
+  end
+
+  def retrieve_from_website
+    5.times do
+      wd = WeatherData.new(@weather_date.strftime('%Y%m%d'))
+      readings = wd.process
+      read = readings.values.map { |data| Reading.new(data) }
+      @readings << read
+      @weather_date += 1.day
+    end
   end
 end
