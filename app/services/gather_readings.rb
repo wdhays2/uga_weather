dbf1a9c4035eae828775fc2ea504ee20f9816840
weshays <weshays@gbdev.com>
2017-10-06 21:31:43 -0700

    Refactored gather_readings.rb

--- app/services/gather_readings.rb
+++ app/services/gather_readings.rb
@@ -12,42 +12,42 @@ class GatherReadings
   end
 
   def run
-    # fetch the data from the database if it exists
+    retrieve_from_website_and_store_in_database unless has_5_days_of_data?
     retrieve_from_database
-    return @readings if @readings.size < 5
-
-    # otherwise, get it from the website
-    retrieve_from_website
-    @readings.reject!(&:empty?)
     @readings
   end
 
+  def has_5_days_of_data?
+    WeatherEntry.select(:entered_on)
+                .where('entered_on between ? AND ?', @start_date, @end_date)
+                .group(:entered_on)
+                .pluck(:entered_on)
+                .size == 5
+  end
+
   def retrieve_from_database
-    ids = WeatherEntry.select(:id)
-                      .where('entered_on between ? AND ?', @start_date, @end_date)
-                      .order(entered_on: :asc)
-                      .pluck(:id)
-    r = ids.map { |id| WeatherEntry.find(id) }
-    method_name(r)
-  end
-
-  def method_name(r)
-    day = @start_date
-    5.times do
-      days_readings = []
-      r.each { |reading| days_readings << reading if reading.entered_on == day }
-      return if days_readings.empty?
-      @readings << days_readings
-      day += 1.day
+    data = {}
+    WeatherEntry.where('entered_on between ? AND ?', @start_date, @end_date)
+                .order(entered_on: :asc, name: :asc).each do |row|
+      data[row.entered_on.to_s] = [] if data[row.entered_on.to_s].nil?
+      data[row.entered_on.to_s] << {
+        name: row.name,
+        max_reading: row.max_reading,
+        max_time: row.max_time,
+        min_reading: row.min_reading,
+        min_time: row.min_time,
+        avg_reading: row.avg_reading
+      }
     end
+    @readings = data.map { |entered_on, wdata| { date: entered_on, data: wdata } }
   end
 
-  def retrieve_from_website
+  def retrieve_from_website_and_store_in_database
     (@start_date..@end_date).each do |date|
+      next if WeatherEntry.where(entered_on: date.to_s).exist?
       wd = WeatherData.new(date.strftime('%Y%m%d'))
       readings = wd.process
-      read = readings.values.map { |data| Reading.new(data) }
-      @readings << read
+      readings.values.each { |data| WeatherEntry.create!(data) }
     end
   end
 end
