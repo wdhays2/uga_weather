4d98c91535ae56c703d09e6fab63093f3f885d74
wdhays2 <wdhays2@charter.net>
2017-10-11 18:58:45 -0700

    WIP

--- db/schema.rb
+++ db/schema.rb
@@ -1,39 +1,36 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema
-# definition.
+# incrementally modify your database, and then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your
 # database schema. If you need to create the application database on another
 # system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more
-# migrations you'll amass, the slower it'll run and the greater likelihood
-# for issues).
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended that you check this file into your version control
-# system.
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_927_165_459) do
+ActiveRecord::Schema.define(version: 20170927165459) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'readings', force: :cascade do |t|
-    t.integer 'max_wind_speed'
-    t.integer 'max_humidity'
-    t.float 'max_temp'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "readings", force: :cascade do |t|
+    t.integer "max_wind_speed"
+    t.integer "max_humidity"
+    t.float "max_temp"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'weather_entries', force: :cascade do |t|
-    t.string 'name'
-    t.float 'max_reading'
-    t.time 'max_time'
-    t.float 'min_reading'
-    t.time 'min_time'
-    t.float 'avg_reading'
-    t.date 'entered_on'
+  create_table "weather_entries", force: :cascade do |t|
+    t.string "name"
+    t.float "max_reading"
+    t.time "max_time"
+    t.float "min_reading"
+    t.time "min_time"
+    t.float "avg_reading"
+    t.date "entered_on"
   end
+
 end
