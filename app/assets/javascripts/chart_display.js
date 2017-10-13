1246878f2cbc74b2a3fbbb168212fe38c645038a
wdhays2 <wdhays2@charter.net>
2017-10-12 17:56:40 -0700

    Minor changes for looks.

--- app/assets/javascripts/chart_display.js
+++ app/assets/javascripts/chart_display.js
@@ -1,8 +1,8 @@
 function weatherDisplay(data) {
   Highcharts.chart(data['section'], {
     chart: { type: 'columnrange', inverted: true },
-    title: { text: data['section'] + ' variation by day +/- 2 days' },
-    subtitle: { text: 'Observed at the University of Georgia' },
+    title: { text: data['section'] + ' Variation' },
+    subtitle: { text: '' },
     xAxis: { categories: data['date_range'] },
     yAxis: { title: { text: data['section'] } },
     tooltip: { valueSuffix: data['tooltip'] },
