9667e1db9b191c6192a7d3f03a20f9da5ecd13ce
wdhays2 <wdhays2@charter.net>
2017-10-16 20:12:36 -0700

    Get calendar working with submit button.

--- app/assets/javascripts/result_display.js
+++ app/assets/javascripts/result_display.js
@@ -8,6 +8,13 @@ $(document).ready(function(){
     changeMonth: true,
     changeYear: true,
     minDate: new Date(2002, 09, 07),
-    maxDate: -1
+    maxDate: -1,
+    dateFormat: 'yymmdd'
   });
 });
+
+// document.getElementById("myDate").addEventListener("select", loadDate);
+
+// function loadDate() {
+//     document.getElementById("datepicker");
+// };
