0a38614dbd082c36b01d2fca14c666b0936daeeb
wdhays2 <wdhays2@charter.net>
2017-10-17 21:00:35 -0700

    WIP

--- app/assets/javascripts/result_display.js
+++ app/assets/javascripts/result_display.js
@@ -8,7 +8,11 @@ $(document).ready(function(){
     changeYear: true,
     minDate: new Date(2002, 09, 07),
     maxDate: -1,
-    dateFormat: 'yymmdd'
+    dateFormat: 'yymmdd',
+    onSelect: function(){
+      // window.location = '/results',
+      console.log('Hey')
+    }
   });
 
 });
